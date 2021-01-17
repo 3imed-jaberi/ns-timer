eb8f5fe8e261d883eba14bc7c1b060003b4038e1
3imed-jaberi <imed_jebari@hotmail.fr>
2021-01-17 21:27:55 +0100

    add src code 🚀 ..

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-# init 🚧 ..
+/*!
+ * ns-timer
+ *
+ * Copyright(c) 2020 Imed Jaberi
+ * MIT Licensed
+ */
+
+'use strict'
+
+/**
+ * Nanoseconds timer for Node.js.
+ *
+ * @api public
+ */
+class Timer {
+  constructor () {
+    this.started = []
+    this.stopped = []
+  }
+
+  // starts a timer.
+  start () {
+    if (this.started.length > 0) {
+      throw new Error('Timer has been started.')
+    }
+
+    this.started = process.hrtime()
+
+    return this
+  }
+
+  // stops a timer.
+  stop () {
+    if (this.started.length === 0) {
+      throw new Error('Timer has not been started.')
+    }
+
+    if (this.stopped.length > 0) {
+      throw new Error('Timer has been stopped.')
+    }
+
+    this.stopped = process.hrtime(this.started)
+
+    return this
+  }
+
+  // the diff nanoseconds between start & stop.
+  diff () {
+    if (this.stopped.length === 0) {
+      throw new Error('Timer has not been stopped.')
+    }
+
+    return this.stopped[0] * 1e9 + this.stopped[1]
+  }
+}
+
+/**
+ * Expose `Timer`.
+ */
+
+module.exports = Timer
