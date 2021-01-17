1dfc53f2ca4b419887b3df95d344b0333b016103
3imed-jaberi <imed_jebari@hotmail.fr>
2021-01-17 21:28:46 +0100

    add test code 🧪 ..

--- index.spec.js
+++ index.spec.js
@@ -1,1 +1,30 @@
-# init 🚧 ..
+const assert = require('assert')
+const Timer = require('.')
+
+describe('Timer', () => {
+  it('start method', () => {
+    const timer = new Timer()
+    assert.strictEqual(timer.started.length, 0)
+    assert.strictEqual(timer.stopped.length, 0)
+    timer.start()
+    assert.notStrictEqual(timer.started.length, 0)
+    assert.strictEqual(timer.stopped.length, 0)
+    assert.throws(() => { timer.start() })
+  })
+
+  it('stop method', () => {
+    const timer = new Timer()
+    assert.throws(() => { timer.stop() })
+    timer.start().stop()
+    assert.notStrictEqual(timer.started.length, 0)
+    assert.notStrictEqual(timer.started.length, 0)
+    assert.throws(() => { timer.stop() })
+  })
+
+  it('diff method', () => {
+    const timer = new Timer()
+    assert.throws(() => { timer.diff() })
+    timer.start().stop()
+    assert.strictEqual(typeof timer.diff(), 'number')
+  })
+})
